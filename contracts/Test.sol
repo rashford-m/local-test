95ecf4b99a0726c2e1d7a0b692dc16e2c88b7adf
Rashford <mikearowo@gmail.com>
2023-09-20 22:05:22 +0100

    new

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -3,9 +3,18 @@ pragma solidity 0.8.8; // Hello all
 
 contract SimpleStorage {
    // boolean, uint, int, address, bytes
-   uint256 FavoriteNumber;
+   uint256 public FavoriteNumber;
+   People public person = People({favoriteNumber: 2, name: "Rashford"})
+
+   struct People {
+    uint256 favoriteNumber;
+    string name;
+   }
 
    function store(uint256 _favoriteNumber) public {
        FavoriteNumber = _favoriteNumber;
    }
 }
+   
+
+   
